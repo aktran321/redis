b3f9ed9a81a0a6716dfd051318f66e91a0e66dec
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-05 09:07:06 -0500

    functionality for handling echo commands and parsing the data

--- app/main.py
+++ app/main.py
@@ -2,24 +2,31 @@
 import socket
 import threading
 
+def parse_resp(data):
+    lines = data.split(b'\r\n')
+    command = lines[2].decode().lower()
+    message = lines[4].decode()
+    return command, message
+
 def handle_client(conn, addr):
     """
     handles a single client connection
     """
-    print("New connection established from {addr}")
+    print(f"New connection established from {addr}")
     while True:
         data = conn.recv(1024)
         if not data:
             break
-        conn.send(b"+PONG\r\n")
+        command, message = parse_resp(data)
+        if command == "echo":
+            response = f"${len(message)}\r\n{message}\r\n"
+            conn.send(response.encode())
     conn.close()
-    print("Connection closed with {addr}")
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     print("server is listening on port 6379")
-    server_socket.listen()
     print("Server is listening for connections")
     
     while True:
