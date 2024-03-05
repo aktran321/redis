ab49bab47b1992b70ec43b0f093b3b912de63776
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-05 08:02:44 -0500

    handle multiple lings

--- app/main.py
+++ app/main.py
@@ -4,9 +4,19 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    print("server is listening on port 6379")
     conn , _ = server_socket.accept()
-    with conn:
+    print("connection established")
+    while True:
+        # receive data from the client
+        data = conn.recv(1024)
+        if not data:
+            # if no data is received, break from the loop
+            break
+        # send response to client
         conn.send(b"+PONG\r\n")
+    conn.close()
+    print("connection closed")
 
 if __name__ == "__main__":
     main()
