c764857242a40755c88c4fba5fb72fb1b5771856
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-05 08:39:16 -0500

    enabled threading

--- app/main.py
+++ app/main.py
@@ -1,22 +1,33 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
+
+def handle_client(conn, addr):
+    """
+    handles a single client connection
+    """
+    print("New connection established from {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.send(b"+PONG\r\n")
+    conn.close()
+    print("Connection closed with {addr}")
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     print("server is listening on port 6379")
-    conn , _ = server_socket.accept()
-    print("connection established")
+    server_socket.listen()
+    print("Server is listening for connections")
+    
     while True:
-        # receive data from the client
-        data = conn.recv(1024)
-        if not data:
-            # if no data is received, break from the loop
-            break
-        # send response to client
-        conn.send(b"+PONG\r\n")
-    conn.close()
-    print("connection closed")
+        conn, addr = server_socket.accept()
+
+        client_thread = threading.Thread(target=handle_client, args = (conn, addr))
+        client_thread.start()
+        print(f"Activate connections: {threading.activeCount() - 1}")
 
 if __name__ == "__main__":
     main()
