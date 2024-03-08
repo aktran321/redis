8a5f4a61531f1b3e65bf599c27298f8653a90d67
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-08 06:20:34 -0500

    auto increment entry sequence

--- app/main.py
+++ app/main.py
@@ -9,7 +9,12 @@ def addDataStream(stream_key, entry_id, *key_value_pairs):
         data_store[stream_key] = {"type": "stream", "value": []}
     last_entry_id = data_store[stream_key]["value"][-1]["id"] if data_store[stream_key]["value"] else "0-0"
     last_ms, last_seq = map(int, last_entry_id.split("-"))
-    current_ms, current_seq = map(int, entry_id.split("-"))
+    current_ms, current_seq = entry_id.split("-")
+    if current_seq == "*":
+        current_seq = last_seq + 1
+    else:
+        current_seq = int(current_seq)
+    current_ms = int(current_ms)
     if current_ms == 0 and current_seq == 0:
         return "-ERR The ID specified in XADD must be greater than 0-0\r\n"
     if current_ms < last_ms or (current_ms == last_ms and current_seq <= last_seq):
