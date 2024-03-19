b223d3d9f2b1cd195025a5f810f187116ddc913b
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-19 12:42:53 -0400

    edit

--- app/main.py
+++ app/main.py
@@ -336,7 +336,8 @@ def handle_client(conn, addr):
             conn.send(response.encode())
             rdb_hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
             length = len(rdb_hex)
-            response = f"${length}\r\n{rdb_hex}"
+            rdb_content = bytes.fromhex(rdb_hex)
+            response = f"${length}\r\n".encode() + rdb_content
             conn.send(response.encode())
         # ====================================================================
         elif command == "replconf":
