6e4bbd80cb51a166f245395893d8cae718cd0dd0
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-03-19 12:38:57 -0400

    send empty file

--- app/main.py
+++ app/main.py
@@ -334,6 +334,10 @@ def handle_client(conn, addr):
             offset = "0"
             response = f"+FULLRESYNC {replid} {offset}\r\n"
             conn.send(response.encode())
+            rdb_hex = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+            length = len(rdb_hex)
+            response = f"${length}\r\n{rdb_hex}"
+            conn.send(response.encode())
         # ====================================================================
         elif command == "replconf":
             response = "+OK\r\n"
